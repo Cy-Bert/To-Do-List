238cf2cffcd134efe06d1a5be8c2d380b5fccc96
Cy-Bert <125924936+Cy-Bert@users.noreply.github.com>
2023-09-05 10:55:18 +0200

    🎨 style(IndexPage.vue): add input field and button to enhance user interaction 🔧 refactor(IndexPage.vue): extract clearMessage, handleKeyup, and alertMessage methods for better code organization and reusability

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,7 @@
 <template>
   <q-page padding>
+    <input v-model="message" @keyup="handleKeyup" />
+    <button @click="clearMessage">Clear</button>
     <h5>{{ message }}</h5>
   </q-page>
 </template>
@@ -12,7 +14,23 @@ export default defineComponent({
     return {
       message: 'I love Vue.js!'
     }
-  }
 
+  },
+  methods: {
+    clearMessage() {
+      this.message = ''
+    },
+    handleKeyup(e) {
+      if (e.keyCode == 27) {
+        this.clearMessage()
+      }
+      else if (e.keyCode == 13) {
+        this.alertMessage()
+      }
+    },
+    alertMessage() {
+      alert(this.message)
+    }
+  }
 })
 </script>
